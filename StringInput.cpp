2f9a9a5d59586742b33006c0b0527e5fae8ac81b
Kyjorn <hendonmshane@gmail.com>
2025-03-30 10:43:57 -0600

    Implement loop for user string input and concatenation in StringInput.cpp

--- StringInput.cpp
+++ StringInput.cpp
@@ -7,4 +7,20 @@ int main () {
     string string1;
     string string2;
     string combo;
+
+    //loop to ask user 3 times for string input
+    for (int i = 1; i <= 3; i++) {
+        
+        cout << "Enter a string: ";     //gets first string
+        getline(cin, string1);
+
+        cout << "Enter another string"; //gets second string
+        getline(cin, string2);
+
+        combo = string1 + string2;      //concatenates the two strings
+
+        cout << combo << endl;          //prints the concatenated string with a new line
+    }
+
+    return 0;    
 }
